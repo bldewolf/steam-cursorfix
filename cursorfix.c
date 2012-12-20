34ac08ae9be59eff46e9d57b9555c0bd5a6c8c5b
Adam Mizerski <adam@mizerski.pl>
2012-12-21 00:07:00 +0100

    Removed messing with LD_LIBRARY_PATH; Created Makefile to take care of dependency source->library->run

--- cursorfix.c
+++ cursorfix.c
@@ -1,9 +1,11 @@
-
 #include <X11/Xcursor/Xcursor.h>
+#ifndef NDEBUG
 #include <stdio.h>
-
+#endif
 
 int XDefineCursor(Display *display, Window w, Cursor cursor) {
-//	printf("Preventing cursor setting\n");
+#ifndef NDEBUG
+	puts("Preventing cursor setting");
+#endif
 	return 0;
 }
