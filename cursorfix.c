640ac5121c5ee642b8c77803783b039d8a2b618e
Brian De Wolf <git@bldewolf.com>
2012-12-31 16:19:13 -0800

    Only prevent updates to the root window
    Since we're mainly upset about steam setting the cursor for the root window, I
    figured it would be a good idea to only prevent those cursor settings (using
    the browser parts of the interface without the cursor changing over buttons is
    a little strange).
    
    However, it's doing more work than I thought. It changes the cursor constantly
    for whatever window the cursor is currently over (well, the steam window or the
    root window, depending on position). In fact, when you're over the steam
    window, waving the mouse causes a flurry of cursor changes (and a little cpu
    usage bump).
    
    I really wish it would just define the cursor over buttons and undefine the
    cursor when it leaves them. I suspect this is to cover up weak coupling between
    steam and the windowed elements (browser, flash, etc) but it's still
    disappointing.

--- cursorfix.c
+++ cursorfix.c
@@ -1,9 +1,20 @@
+#define _GNU_SOURCE
 
+#include <dlfcn.h>
 #include <X11/Xcursor/Xcursor.h>
 #include <stdio.h>
 
-
 int XDefineCursor(Display *display, Window w, Cursor cursor) {
+	int (*orig_XDefineCursor)(Display *display, Window w, Cursor cursor);
+	Window root;
 //	printf("Preventing cursor setting\n");
+	root = DefaultRootWindow(display);
+	if(w == root) {
+		puts("Trying to set cursor for root window");
+	} else {
+		puts("Setting cursor for non-root window");
+		orig_XDefineCursor = dlsym(RTLD_NEXT, "XDefineCursor");
+		return orig_XDefineCursor(display, w, cursor);
+	}
 	return 0;
 }
