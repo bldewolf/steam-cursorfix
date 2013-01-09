69e1801b53876977da68d041958a9492b2844300
Brian De Wolf <git@bldewolf.com>
2013-01-08 18:56:52 -0800

    Use XUndefineCursor when appropriate
    Steam still assumes that XC_arrow is the default cursor it should have when
    nothing context-specific is being hovered over. Instead, it should inherit the
    default cursor by using XUndefineCursor. Since the cursors used by
    XDefineCursor seem to get re-used, we can snag the XID of the XC_arrow cursor
    and secretly use XUndefineCursor instead when Steam sets the cursor to it. This
    preserves both context-sensitive cursors and the default cursor from the root
    window.

--- cursorfix.c
+++ cursorfix.c
@@ -3,18 +3,42 @@
 #include <dlfcn.h>
 #include <X11/Xcursor/Xcursor.h>
 #include <stdio.h>
+#include <X11/cursorfont.h>
+
+Cursor default_cursor = -1;
+
+Cursor XCreateFontCursor(Display *display, unsigned int shape) {
+	Cursor cursor;
+	Cursor (*orig_XCreateFontCursor)(Display *display, unsigned int shape);
+
+	orig_XCreateFontCursor = dlsym(RTLD_NEXT, "XCreateFontCursor");
+	cursor = orig_XCreateFontCursor(display, shape);
+
+	if(shape == XC_arrow)
+		default_cursor = cursor;
+
+//	printf("Set shape %d to XID %d\n", shape, cursor);
+	return cursor;
+}
+
 
 int XDefineCursor(Display *display, Window w, Cursor cursor) {
 	int (*orig_XDefineCursor)(Display *display, Window w, Cursor cursor);
 	Window root;
-//	printf("Preventing cursor setting\n");
 	root = DefaultRootWindow(display);
 	if(w == root) {
+		// Yay, this never happens anymore
 		puts("Trying to set cursor for root window");
 	} else {
-		puts("Setting cursor for non-root window");
-		orig_XDefineCursor = dlsym(RTLD_NEXT, "XDefineCursor");
-		return orig_XDefineCursor(display, w, cursor);
+//		printf("Cursor id %d\n", cursor);
+		if(cursor != default_cursor) {
+//			puts("Setting cursor for non-root window");
+			orig_XDefineCursor = dlsym(RTLD_NEXT, "XDefineCursor");
+			return orig_XDefineCursor(display, w, cursor);
+		} else {
+//			puts("Unsetting cursor for non-root window");
+			XUndefineCursor(display, w);
+		}
 	}
 	return 0;
 }
